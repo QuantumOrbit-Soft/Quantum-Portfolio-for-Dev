64d2164237aa919d2e2fb34b9b70c6d6970bc5a4
rapdos <raphael.santos.esteves@gmail.com>
2026-05-15 16:24:12 -0300

    fix import to index.css and tab title

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Quantum-Portfolio-for-Dev/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
